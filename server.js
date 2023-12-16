5452aeea2d32cd7d93853e9aa5a79626728504bc
Nadir Bourazza <NadirBourazza@tfl.gov.uk>
2023-12-16 02:53:02 +0000

    Expanded Query to include rollDice, which accepts arguments

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var schema = buildSchema(`
     quoteOfTheDay: String
     random: Float!
     rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }`
 );
 
@@ -21,6 +22,13 @@ var root = {
   },
   rollThreeDice: () => {
     return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   }
 }
 
