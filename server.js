1c924a7920e1e06364868c8e021527062c66eb93
Nadir Bourazza <NadirBourazza@tfl.gov.uk>
2023-12-16 15:53:39 +0000

    Implemented mutation logic example

--- server.js
+++ server.js
@@ -4,41 +4,61 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput { 
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
   }
 `);
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides)
-  }
-
-  roll({numRolls}) {
-    var output = []
-    for (let i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce())
-    }
-    return output
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}){
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
-
+var fakeDatabase = {};
 
 // Provide resolver function for each API endpoint
 var root = {
-  getDie:({numSides}) => new RandomDie(numSides || 6)
+  createMessage: ({input}) => {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({id, input}) => {
+    if(!fakeDatabase[id]){
+      throw new Error('No message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  getMessage: ({id}) => {
+    if(!fakeDatabase[id]){
+      throw new Error('No message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  }
 }
 
 var app = express();
